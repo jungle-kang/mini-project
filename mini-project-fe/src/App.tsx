f0342aa25399be4bc606410c27d18a848e2f2308
ljo1010 <ljo1010@gmail.com>
2024-04-26 15:20:11 +0900

    이제 리스트 만들면 업로드 됨.

--- mini-project-fe/src/App.tsx
+++ mini-project-fe/src/App.tsx
@@ -3,12 +3,18 @@ import Modal from './Modal';
 
 const App = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [roomTitle, setRoomTitle] = useState('');
+  const [roomPeople, setRoomPeople] = useState('');
+  const [roomList, setRoomList] = useState([]);
 
   const handleCreateRoom = (title, people) => {
     console.log('방 제목:', title);
     console.log('인원 수:', people);
     // TODO: 방 생성 로직 구현
     setModalOpen(false);
+    setRoomTitle(title); // 방 제목 업데이트
+    setRoomPeople(people); // 인원 수 업데이트
+    setRoomList([{ title, people }, ...roomList]); // 새로운 방을 방 리스트의 맨 앞에 추가
   };
 
   return (
@@ -20,7 +26,12 @@ const App = () => {
       </div>
     
       <div className='roomList'>
-        <h4>방이름</h4>
+        {roomList.map((room, index) => (
+          <div key={index}>
+            <h4>{room.title}</h4> {/* 방 제목 출력 */}
+            <p>{room.people}명</p> {/* 인원 수 출력 */}
+          </div>
+        ))}
       </div>
       <Modal
         isOpen={modalOpen}
